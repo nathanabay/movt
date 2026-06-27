1e744cede05ca898e6b5df6addb1bb31224ef575
nathan amare <NATHANAMARE@HOTMAIL.COM>
2026-06-27 20:35:35 +0300

    Fix Express 5 wildcard routing error

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use('/api/yts', createProxyMiddleware({
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback for React Router - ALWAYS return index.html for unknown routes
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
